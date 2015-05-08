736b2ffb2e436c8fbc0626d8546e94a989d0b295
akhaybullin <18akhaybullin@iaa.edu.ec>
2015-05-07 21:38:44 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,20 @@
+name = "Artem" #string 
+lastname = "Khaybullin" 
+adress = "Cdl. Rio Guayas Club" 
+email = "akhaybullin@gmail.com" 
+phonenumber = "0993242051" 
+
+print name 
+print lastname 
+print adress
+print email 
+print phonenumber
+
+print "My name is", name, lastname
+
+print "My adress is", adress
+
+print "My email is", email
+
+print "My phone number is", phonenumber
+
