89d217d81fe3658010357292c6891f406bef875f
akhaybullin <18akhaybullin@iaa.edu.ec>
2015-05-07 21:37:17 -0500

    Create Classwork.py

---
+++ Classwork.py
@@ -0,0 +1,15 @@
+mystring = "Hello" 
+myfloat = 10 
+myint = 20 
+
+print mystring
+print myfloat 
+print myint 
+
+print mystring,"my name is Artem, i am", myfloat "years old, and i will be", myint, "soon"
+
+print 5*4/3 
+print 34+12/5 
+print 23-5/8 
+print 1+3*5+3/9 
+print 1212/2345+6  
