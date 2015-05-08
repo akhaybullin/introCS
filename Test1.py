6b0c516bc69af62640aef1fca483a650121f05ae
akhaybullin <18akhaybullin@iaa.edu.ec>
2015-05-07 21:39:17 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,11 @@
+operand1 = 95 
+operand2 = 64.5 
+
+print operand1 + operand2 
+print operand1 - operand2
+print operand1 * operand2
+print operand1 / operand2
+print operand1 % operand2 
+
+#I created an integer and a floating point number  
+#Then i replaced the numbers with the floating points and integers
