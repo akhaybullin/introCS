b5154e68f467babeb9a74f23329b55b742623097
akhaybullin <18akhaybullin@iaa.edu.ec>
2015-05-07 21:40:20 -0500

    Create Variables1.py

---
+++ Variables1.py
@@ -0,0 +1,26 @@
+name = "Artem Khaybullin" #string 
+age = 33 #integer 
+gpa = 4.0 #floating point 
+
+print name
+print age 
+print gpa 
+
+print "My name is", name, ". I am ", age, "years old. My gpa is", gpa
+
+print "In ten years, I will be", age + 10 
+
+#Dog Years 
+ageInHumanYears = 3 
+ageInDogYears = ageInHumanYears * 7 
+
+print "My dog is", ageInDogYears, "years old in Dog Years." 
+
+myNumber = 5 + 4 * 5 
+print myNumber 
+
+print "Hello", 2
+
+import math 
+root = math.sqrt(81) 
+print root
