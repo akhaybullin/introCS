98c295993b22c9e62147a244fa86dd1d66424894
akhaybullin <18akhaybullin@iaa.edu.ec>
2015-05-07 21:35:57 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,20 @@
+print 'Hello World'
+print 'My Name is Artem' 
+print 'I am 14 years old \nThis is Python' 
+
+print 2015 + 1 #Printing an integer
+print '2015'
+
+print 3.1416 #Printing a floating point number
+
+print 2015 - 1 #Printing an integer 
+
+print 2015 * 2 #Printing an integer
+
+print 81 / 9 #Printing an integer 
+
+#Module
+print 10%3 
+
+print 17 % 2 
+print 16 % 2 
