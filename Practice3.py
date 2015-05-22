aa260c80544e9cd6fc2e5ec7a9d6032752d58e76
akhaybullin <18akhaybullin@iaa.edu.ec>
2015-05-22 10:13:34 -0500

    Create Practice3.py

---
+++ Practice3.py
@@ -0,0 +1,17 @@
+name = raw_input("What is your name? ")
+last = raw_input("What is your last name? ")
+birth = input("What year were you born? ")
+grade = input("What is your grade? ")
+
+age = 2015 - birth 
+print age  
+
+Computer1 = input("What is your computer grade for first semester? ")
+Computer2 = input("What is your computer grade for second semester? ")
+
+Finalgrade = (Computer1 + Computer2) / 2 
+
+print "Your name is", name
+print "Your last name is", last
+print "Your class is", grade 
+print "Your final grade is", Finalgrade
