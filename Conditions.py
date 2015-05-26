d9286204273cd6acb740d43d39defe331ea67abf
akhaybullin <18akhaybullin@iaa.edu.ec>
2015-05-26 10:32:02 -0500

    Create Conditions.py

---
+++ Conditions.py
@@ -0,0 +1,8 @@
+print "Bar" 
+age = input("What is the age of the customer? ") 
+
+if age>= 18: 
+    print "The customers is legally to drink alcohol"
+    print "You may serve" 
+else: 
+    print "The customer is NOT legally allowed to drink alcohol" 
